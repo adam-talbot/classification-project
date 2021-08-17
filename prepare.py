7aa40efc708a2c2649dea03e4ba63fa6164a35d0
adam-talbot <adam.talbot527@gmail.com>
2021-08-17 17:08:45 -0500

    modified prepare module so that two functions were combined into single function to do all prep (cleaning, dummying, and splitting)

--- prepare.py
+++ prepare.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-def prepare_telco(df):
+def clean_telco(df):
     df.drop(columns = ['payment_type_id', 'contract_type_id', 'internet_service_type_id', 'customer_id'], inplace=True)
     df.total_charges.replace(to_replace={' ' : '0'}, inplace = True)
     df.total_charges = df.total_charges.astype('float')
@@ -16,4 +16,9 @@ def dummy_and_split(df):
     df_w_dummies.drop(columns=cat_cols, inplace=True)
     train, test = train_test_split(df_w_dummies, test_size=.2, random_state=123, stratify=df_w_dummies.churn_Yes)
     train, validate = train_test_split(train, test_size=.25, random_state=123, stratify=train.churn_Yes)
+    return train, validate, test
+
+def prepare_telco(df):
+    df = clean_telco(df)
+    train, validate, test = dummy_and_split(df)
     return train, validate, test
